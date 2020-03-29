73ebe2814c82e52db3d0f75e40af4f51f4a72040
MadShadow- <Mad.Shadow@web.de>
2020-03-29 04:35:05 +0200

    updated reconnect tool
    work properly again and quicker

--- EMS/tools/ReconnectTool.lua
+++ EMS/tools/ReconnectTool.lua
@@ -15,46 +15,28 @@ ReconnectTool = {
 			ReconnectTool.Started = true;
 			ReconnectTool.Text = " Verbinden ";
 			XGUIEng.ShowWidget("ReconnectToolState",1);
+			XNetworkUbiCom.Manager_LogIn_Connect();
+			XNetworkUbiCom.Manager_LogIn_Start();
 			return 1;
 		end,
 		
-		[2] = XNetworkUbiCom.Manager_LogIn_Connect,
-		[3] = XNetworkUbiCom.Manager_LogIn_Start,
-		
-		[4] = function()
-			return XNetworkUbiCom.Lobby_Group_Enter(NETWORK_GAME_LOBBY);
-		end,
-		
-		[5] = function()
+		[2] = function()
 			XNetworkUbiCom.Lobby_Group_Enter(NETWORK_GAME_LOBBY);
 			if XNetworkUbiCom.Lobby_Group_GetIndexOfCurrent() == -1 then
 				ReconnectTool.GameNotOpenAnymore = true;
 				return 1;
 			end
-			XNetwork.Chat_SendMessageToAll(".join"); return 1;
-		end,
-		
-		[6] = function()
+			XNetwork.Chat_SendMessageToAll(".join");
 			CNetwork.set_ready();
-			return 1;
-		end,
-		
-		[7] = function()
 			CNetwork.send_need_ticks(Network_GetLastTickReceived() + 2000);
 			-- reset params
 			ReconnectTool.Started = false;
 			ReconnectTool.CurrentActionIndex = 0;
 			-- update GUI
 			ReconnectTool.Text = " @color:0,255,0 Verbunden! ";
-			local count = 3;
-			ReconnectTool_Hide = function()
-				if count > 0 then count = count - 1; return; end
-				XGUIEng.ShowWidget("ReconnectToolState",0);
-				return true;
-			end
-			StartSimpleJob("ReconnectTool_Hide");
 			XGUIEng.SetText( "ReconnectToolState", ReconnectTool.Text);
 			XNetwork.Manager_Create();
+			XGUIEng.ShowWidget("ReconnectToolState",0);
 			return 1;
 		end,
 	},
@@ -73,13 +55,13 @@ function ReconnectTool.UpdateEveryFrame()
 		end
 	end
 	if ReconnectTool.GameNotOpenAnymore then
-		XGUIEng.SetText( "ReconnectToolState", " @color:255,0,0 Spiel nicht mehr offen! Kann nicht verbinden! ");
+		XGUIEng.SetText( "ReconnectToolState", " @color:255,0,0 Spiel auf Kimichuras Server nicht mehr offen! Kann nicht verbinden! ");
 		return;
 	end
 	-- disconnected => reconnect
 	local currentTimestamp = math.floor(XGUIEng.GetSystemTime());
 	local delay = currentTimestamp - ReconnectTool.LastActionTimestamp;
-	XGUIEng.SetText( "ReconnectToolState", "("..ReconnectTool.CurrentActionIndex.."/7) "..ReconnectTool.Text.." in "..(ReconnectTool.DelayNextAction-delay));
+	XGUIEng.SetText( "ReconnectToolState", "("..ReconnectTool.CurrentActionIndex.."/"..table.getn(ReconnectTool.Actions).." "..ReconnectTool.Text.." in "..(ReconnectTool.DelayNextAction-delay));
 	if delay > ReconnectTool.DelayNextAction then
 		-- update timestamp
 		ReconnectTool.LastActionTimestamp = currentTimestamp;
