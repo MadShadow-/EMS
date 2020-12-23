13d6e2c73e0e2a491ca42b2ba59a7c01edcd6745
MadShadow- <Mad.Shadow@web.de>
2020-12-23 23:50:13 +0100

    Map (8) EMS Berginselkampf added

---
+++ EMSconfigs/(8) ems berginselkampf.lua
@@ -0,0 +1,127 @@
+-- ************************************************************************************************
+-- *                                                                                              *
+-- *                                                                                              *
+-- *                                              EMS                                             *
+-- *                                         CONFIGURATION                                        *
+-- *                                                                                              *
+-- *                                                                                              *
+-- ************************************************************************************************
+ 
+EMS_CustomMapConfig =
+{
+	-- ********************************************************************************************
+	-- * Configuration File Version
+	-- * A version check will make sure every player has the same version of the configuration file
+	-- ********************************************************************************************
+	Version = 1,
+
+	-- ********************************************************************************************
+	-- * Callback_OnMapStart
+	-- * Called directly after the loading screen vanishes and works as your entry point.
+	-- * Similar use to FirstMapAction/GameCallback_OnGameSart
+	-- ********************************************************************************************
+ 
+	Callback_OnMapStart = function()
+		AddPeriodicSummer(60);
+		SetupHighlandWeatherGfxSet();
+		LocalMusic.UseSet = HIGHLANDMUSIC;
+	end,
+ 
+ 
+	-- ********************************************************************************************
+	-- * Callback_OnGameStart
+	-- * Called at the end of the 10 seconds delay, after the host chose the rules and started
+	-- ********************************************************************************************
+	Callback_OnGameStart = function()
+	end,
+ 
+	-- ********************************************************************************************
+	-- * Callback_OnPeacetimeEnded
+	-- * Called when the peacetime counter reaches zero
+	-- ********************************************************************************************
+	Callback_OnPeacetimeEnded = function()
+		for i = 1,4 do
+			DestroyEntity("stein"..i);
+		end
+	end,
+ 
+ 
+	-- ********************************************************************************************
+	-- * Peacetime
+	-- * Number of minutes the players will be unable to attack each other
+	-- ********************************************************************************************
+	Peacetime = 20,
+ 
+	-- ********************************************************************************************
+	-- * GameMode
+	-- * GameMode is a concept of a switchable option, that the scripter can freely use
+	-- *
+	-- * GameModes is a table that contains the available options for the players, for example:
+	-- * GameModes = {"3vs3", "2vs2", "1vs1"},
+	-- *
+	-- * GameMode contains the index of selected mode by default - ranging from 1 to X
+	-- *
+	-- * Callback_GameModeSelected
+	-- * Lets the scripter make changes, according to the selected game mode.
+	-- * You could give different ressources or change the map environment accordingly
+	-- * _gamemode contains the index of the selected option according to the GameModes table
+	-- ********************************************************************************************
+	GameMode = 1,
+	GameModes = {"Standard"},
+	Callback_GameModeSelected = function(_gamemode)
+	end,
+	
+	-- ********************************************************************************************
+	-- * Resource Level
+	-- * Determines how much ressources the players start with
+	-- * 1 = Normal
+	-- * 2 = FastGame
+	-- * 3 = SpeedGame
+	-- * See the ressources table below for configuration
+	-- ********************************************************************************************
+	ResourceLevel = 3,
+ 
+	-- ********************************************************************************************
+	-- * Resources
+	-- * Order:
+	-- * Gold, Clay, Wood, Stone, Iron, Sulfur
+	-- * Rules:
+	-- * 1. If no player is defined, default values are used
+	-- * 2. If player 1 is defined, these ressources will be used for all other players too
+	-- * 3. Use the players index to give ressources explicitly
+	-- ********************************************************************************************
+	Ressources =
+	{
+		SpeedGame = {
+			[1] = {
+				20000,
+				20000,
+				20000,
+				20000,
+				20000,
+				20000,
+			},
+		},
+	},
+ 
+	-- ********************************************************************************************
+	-- * Callback_OnFastGame
+	-- * Called together with Callback_OnGameStart if the player selected ResourceLevel 2 or 3
+	-- * (FastGame or SpeedGame)
+	-- ********************************************************************************************
+	Callback_OnFastGame = function()
+	end,
+	
+	Cannon1 = 1,
+	Cannon2 = 1,
+	Cannon3 = 1,
+	Cannon4 = 1,
+	
+	HeavyCavalry = 2,
+	LightCavalry = 2,
+	
+	TowerLevel = 3,
+	TowerLimit = 2,
+	Markets = 0,
+
+};
