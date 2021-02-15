8ad59a59afefc62d078c8cfa5e43188c0ce6c7d0
MadShadow- <Mad.Shadow@web.de>
2021-02-15 14:35:03 +0100

    New version numbers

--- EMSconfigs/(4) EMS Interessenkonflikt.lua
+++ EMSconfigs/(4) EMS Interessenkonflikt.lua
@@ -13,7 +13,7 @@ EMS_CustomMapConfig =
 	-- * Configuration File Version
 	-- * A version check will make sure every player has the same version of the configuration file
 	-- ********************************************************************************************
-	Version = 1.24,
+	Version = 1.26,
  
 	-- ********************************************************************************************
 	-- * Callback_OnMapStart
