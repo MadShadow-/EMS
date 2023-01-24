4e562c26e87ef8aaa50cd9cefee50bef1f2cb701
RomaInvictae <121254963+RomaInvictae@users.noreply.github.com>
2023-01-24 21:43:33 +0100

    VQ 12 Map hinzugefügt

---
+++ EMSconfigs/(12) ems verdorbenes land.lua
@@ -0,0 +1,378 @@
+-- ************************************************************************************************
+-- *                                                                                              *
+-- *                                                                                              *
+-- *                                              EMS                                             *
+-- *                                         CONFIGURATION                                        *
+-- *                                                                                              *
+-- *                                                                                              *
+-- ************************************************************************************************
+
+EMS_CustomMapConfig =
+{
+	-- ********************************************************************************************
+	-- * Configuration File Version
+	-- * A version check will make sure every player has the same version of the configuration file
+	-- ********************************************************************************************
+	Version = 1.0,
+ 
+	-- ********************************************************************************************
+	-- * Debug Mode
+	-- * Activates the ems debug mode if set to true.
+	-- * This will enable key bindings to achieve some common debug tasks.
+	-- * Ingame a button with the text "Debug" will be shown to give further information.
+	-- * (Default: false)
+	-- ********************************************************************************************
+	ActivateDebug = false,
+ 
+	-- ********************************************************************************************
+	-- * Custom debug functions
+	-- * If ActivateDebug is set to true, these two debug methods can be called by pressing N or M
+	-- * _fromPlayer - the id of the player that pressed the key
+	-- * _targetPlayerId1 - a player id between 1-8 (or 16 on cnetwork), that the caller wants to target
+	-- * _targetPlayerId2 - a second player id
+	-- * _x, _y - the position of the callers mouse at the time of pressing the key
+	-- * Example content:
+	-- * SetFriendly(_targetPlayerId1, _targetPlayerId2); -- make friends between p1 and p2
+	-- * Logic.CreateEntity(Entities.PU_Serf, _x, _y, 0, _fromPlayer); -- create Serf for caller
+	-- ********************************************************************************************
+	CustomDebugFunc1 = function(_fromPlayer, _targetPlayerId1, _targetPlayerId2, _x, _y)
+		Logic.CreateEntity(Entities.PU_Serf, _x, _y, 0, _fromPlayer);
+	end,
+	CustomDebugFunc2 = function(_fromPlayer, _target1, _target2, _x, _y)
+		Logic.CreateEntity(Entities.PU_Serf, _x, _y, 0, _target2);
+	end,
+ 
+	-- ********************************************************************************************
+	-- * Callback_OnMapStart
+	-- * Called directly after the loading screen vanishes and works as your entry point.
+	-- * Similar use to FirstMapAction/GameCallback_OnGameSart
+	-- ********************************************************************************************
+	Callback_OnMapStart = function()
+
+		AddPeriodicSummer(60*240)
+		AddPeriodicRain(60*240)
+		SetupHighlandWeatherGfxSet();
+
+		LocalMusic.UseSet = EVELANCEMUSIC;
+
+
+		MapTools.WallsToPlayerZero(Entities.XD_DarkWallStraightGate,
+		Entities.XD_DarkWallStraightGate_Closed, 
+		Entities.XD_DarkWallCorner, 
+		Entities.XD_DarkWallStraight, 
+		Entities.XD_DarkWallDistorted,
+		Entities.XD_WallStraightGate,
+		Entities.XD_WallStraightGate_Closed, 
+		Entities.XD_WallCorner, 
+		Entities.XD_WallStraight, 
+		Entities.XD_WallDistorted)
+
+
+
+
+	
+		local resourceTable = {
+			{Entities.XD_StonePit1, 100000},
+			{Entities.XD_IronPit1, 100000},
+			{Entities.XD_ClayPit1, 100000},
+			{Entities.XD_SulfurPit1, 100000},
+			{Entities.XD_Stone1, 3000},
+			{Entities.XD_Iron1, 3000},
+			{Entities.XD_Clay1, 3000},
+			{Entities.XD_Sulfur1, 3000}
+		}
+		MapTools.SetMapResource(resourceTable);
+		
+	end,
+ 
+	-- ********************************************************************************************
+	-- * Callback_OnGameStart
+	-- * Called at the end of the 10 seconds delay, after the host chose the rules and started
+	-- ********************************************************************************************
+	Callback_OnGameStart = function()
+		for i=1,12 do
+        	CreateWoodPile( "wood"..i, 1000000 )
+		end
+
+		Walltable = {}
+		local pos = GetPosition("wallstart1")
+		for offX=0,10500,750 do
+			table.insert(Walltable,Logic.CreateEntity(Entities.XD_Palisade2,pos.X+offX,pos.Y,90,0))
+			LuaDebugger.Log(GetPosition("wallstart1").X+offX)
+		end
+		local pos = GetPosition("wallstart2")
+		for offX=0,10500,750 do
+			table.insert(Walltable,Logic.CreateEntity(Entities.XD_Palisade2,pos.X-offX,pos.Y,90,0))
+		end
+		local pos = GetPosition("wallstart3")
+		for offX=0,10500,750 do
+			table.insert(Walltable,Logic.CreateEntity(Entities.XD_Palisade2,pos.X+offX,pos.Y,90,0))
+		end
+		local pos = GetPosition("wallstart4")
+		for offX=0,10500,750 do
+			table.insert(Walltable,Logic.CreateEntity(Entities.XD_Palisade2,pos.X-offX,pos.Y,90,0))
+		end
+		
+		
+
+		ThemeMusicTimer = ((EMS_CustomMapConfig.Peacetime * 60)-127)
+			StartSimpleJob("ThemeMusic")
+		
+
+	end,
+	
+ 
+	-- ********************************************************************************************
+	-- * Callback_OnPeacetimeEnded
+	-- * Called when the peacetime counter reaches zero
+	-- ********************************************************************************************
+	Callback_OnPeacetimeEnded = function()
+		for i=1,table.getn(Walltable),1 do
+			DestroyEntity(Walltable[i])
+		end
+
+	end,
+
+ 
+	-- ********************************************************************************************
+	-- * Peacetime
+	-- * Number of minutes the players will be unable to attack each other
+	-- ********************************************************************************************
+	Peacetime = 3,
+ 
+	-- ********************************************************************************************
+	-- * GameMode
+	-- * GameMode is a concept of a switchable option, that the scripter can freely use
+	-- *
+	-- * GameModes is a table that contains the available options for the players, for example:
+	-- * GameModes = {"3vs3", "2vs2", "1vs1"},
+	-- *
+	-- * GameMode contains the index of selected mode by default - ranging from 1 to X
+	-- *
+	-- * Callback_GameModeSelected
+	-- * Lets the scripter make changes, according to the selected game mode.
+	-- * You could give different ressources or change the map environment accordingly
+	-- * _gamemode contains the index of the selected option according to the GameModes table
+	-- ********************************************************************************************
+	GameMode = 1,
+	GameModes = {"Standard"},
+	Callback_GameModeSelected = function(_gamemode)
+	end,
+ 
+	-- ********************************************************************************************
+	-- * Resource Level
+	-- * Determines how much ressources the players start with
+	-- * 1 = Normal
+	-- * 2 = FastGame
+	-- * 3 = SpeedGame
+	-- * See the ressources table below for configuration
+	-- ********************************************************************************************
+	ResourceLevel = 1,
+ 
+	-- ********************************************************************************************
+	-- * Resources
+	-- * Order:
+	-- * Gold, Clay, Wood, Stone, Iron, Sulfur
+	-- * Rules:
+	-- * 1. If no player is defined, default values are used
+	-- * 2. If player 1 is defined, these ressources will be used for all other players too
+	-- * 3. Use the players index to give ressources explicitly
+	-- ********************************************************************************************
+	Ressources =
+	{
+		-- * Normal default: 1k, 1.8k, 1.5k, 0.8k, 50, 50
+		Normal = {
+			[1] = {
+				750,
+				2000,
+				1700,
+				1000,
+				150,
+				150,
+			},
+		},
+		-- * FastGame default: 2 x Normal Ressources
+		FastGame = {},
+ 
+		-- * SpeedGame default: 20k, 12k, 14k, 10k, 7.5k, 7.5k
+		SpeedGame = {},
+	},
+ 
+	-- ********************************************************************************************
+	-- * Callback_OnFastGame
+	-- * Called together with Callback_OnGameStart if the player selected ResourceLevel 2 or 3
+	-- * (FastGame or SpeedGame)
+	-- ********************************************************************************************
+	Callback_OnFastGame = function()
+	end,
+ 
+	-- ********************************************************************************************
+	-- * AI Players
+	-- * Player Entities that belong to an ID that is also present in the AIPlayers table won't be
+	-- * removed
+	-- ********************************************************************************************
+	AIPlayers = {},
+ 
+	-- ********************************************************************************************
+	-- * DisableInitCameraOnHeadquarter
+	-- * Set to true if you don't want the camera to be set to the headquarter automatically
+	-- * (default: false)
+	-- ********************************************************************************************
+	DisableInitCameraOnHeadquarter = false,
+ 
+	-- ********************************************************************************************
+	-- * DisableSetZoomFactor
+	-- * If set to false, ZoomFactor will be set to 2 automatically
+	-- * Set to true if nothing should be done
+	-- * (default: false)
+	-- ********************************************************************************************
+	DisableSetZoomFactor = false,
+ 
+	-- ********************************************************************************************
+	-- * DisableStandardVictoryCondition
+	-- * Set to true if you want to implement your own victory condition
+	-- * Otherwise the player will lose upon losing his headquarter
+	-- * (default: false)
+	-- ********************************************************************************************
+	DisableStandardVictoryCondition = false,
+ 
+	-- ********************************************************************************************
+	-- * Units
+	-- * Various units can be allowed or forbidden
+	-- * A 0 means the unit is forbidden - a higher number represents the maximum allowed level
+	-- * Example: 
+	-- * Sword = 0, equals Swords are forbidden
+	-- * Sword = 2, equals the maximum level for swords is 2 = Upgrading once
+	-- ********************************************************************************************
+	Sword   = 4,
+	Bow     = 4,
+	PoleArm = 4,
+	HeavyCavalry = 2,
+	LightCavalry = 2,
+	Rifle = 2,
+	Thief = 1,
+	Scout = 1,
+	Cannon1 = 1,
+	Cannon2 = 1,
+	Cannon3 = 1,
+	Cannon4 = 1,
+ 
+	-- * Buildings
+	Bridge = 0,
+ 
+	-- * Markets
+	-- * -1 = Building markets is forbidden
+	-- * 0 = Building markets is allowed
+	-- * >0 = Markets are allowed and limited to the number given
+	Markets = -1,
+ 
+	-- * Trade Limit
+	-- * 0 = no trade limit
+	-- * greater zero = maximum amount that you can buy in one single trade 
+	TradeLimit = 0,
+ 
+	-- * TowerLevel
+	-- * 0 = Towers forbidden
+	-- * 1 = Watchtowers
+	-- * 2 = Balistatowers
+	-- * 3 = Cannontowers
+	TowerLevel = 3, -- 0-3
+ 
+	-- * TowerLimit
+	-- * 0  = no tower limit
+	-- * >0 = towers are limited to the number given
+	TowerLimit = 6,
+ 
+	-- * WeatherChangeLockTimer
+	-- * Minutes for how long the weather can't be changed directly again after a weatherchange happened
+	WeatherChangeLockTimer =  3,
+ 
+	-- * Enables chaning to a specific weather with the weather tower
+	MakeSummer = 1,
+	MakeRain   = 1,
+	MakeSnow   = 1,
+ 
+	-- * Fixes the DestrBuild bug
+	AntiBug    = 1,
+ 
+	-- * HQRush
+	-- * If set to 1, Headquarters are invulernerable as long the player still has village centers
+	AntiHQRush = 1,
+ 
+	-- * If set to 1, Players can't abuse blessing and overtime in combination for unlimited work
+	BlessLimit = 1,
+ 
+	-- * if set to true, Players are not able to lose their Headquarter.
+	InvulnerableHQs = false,
+ 
+	-- * Heroes
+	-- * NumberOfHeroesForAll sets the number of heroes every player can pick
+	-- * 1 behind each hero defines if the hero is allowed; 0 for forbidden
+	NumberOfHeroesForAll = 1,
+	Dario    = 1,
+	Pilgrim  = 1,
+	Ari      = 1,
+	Erec     = 0,
+	Salim    = 1,
+	Helias   = 1,
+	Drake    = 0,
+	Yuki     = 0,
+	Kerberos = 1,
+	Varg     = 1,
+	Mary_de_Mortfichet = 1,
+	Kala     = 1,
+};
+
+function ThemeMusic()
+	ThemeMusicTimer = ThemeMusicTimer - 1
+	Message("Timer: " .. ThemeMusicTimer)
+	if ThemeMusicTimer <= 0 then
+	 	StartMusic("02_MainTheme2.mp3", 127)
+		return true
+	end 
+end
+
+
+
+
+--------------------------------------WoodPiles--------------------------------------------------
+
+function CreateWoodPile( _posEntity, _resources )
+    assert( type( _posEntity ) == "string" )
+    assert( type( _resources ) == "number" )
+    gvWoodPiles = gvWoodPiles or {
+        JobID = Trigger.RequestTrigger(Events.LOGIC_EVENT_EVERY_SECOND,nil,"ControlWoodPiles",0,nil,nil),
+    }
+    local pos = GetPosition( _posEntity )
+    local pile_id = Logic.CreateEntity( Entities.XD_Rock3, pos.X, pos.Y, 0, 0 )
+
+    SetEntityName( pile_id, _posEntity.."_WoodPile" )
+
+    local newE = ReplaceEntity( _posEntity, Entities.XD_ResourceTree )
+    Logic.SetModelAndAnimSet(newE, Models.XD_SignalFire1)
+    Logic.SetResourceDoodadGoodAmount( GetEntityId( _posEntity ), _resources*10 )
+    Logic.SetModelAndAnimSet(pile_id, Models.Effects_XF_ChopTree)
+    table.insert( gvWoodPiles, { ResourceEntity = _posEntity, PileEntity = _posEntity.."_WoodPile", ResourceLimit = _resources*9 } )
+end
+
+function ControlWoodPiles()
+    for i = table.getn( gvWoodPiles ),1,-1 do
+        if Logic.GetResourceDoodadGoodAmount( GetEntityId( gvWoodPiles[i].ResourceEntity ) ) <= gvWoodPiles[i].ResourceLimit then
+            DestroyWoodPile( gvWoodPiles[i], i )
+        end
+    end
+end
+
+function DestroyWoodPile( _piletable, _index )
+    local pos = GetPosition( _piletable.ResourceEntity )
+    DestroyEntity( _piletable.ResourceEntity )
+    DestroyEntity( _piletable.PileEntity )
+    Logic.CreateEffect( GGL_Effects.FXCrushBuilding, pos.X, pos.Y, 0 )
+    table.remove( gvWoodPiles, _index )
+end
+
+---------------------------------------------------------------------------------------------------------------------------------------
+function StartMusic(_song, _length)
+    Sound.StartMusic(Folders.Music .. _song, 127)
+    LocalMusic.SongLength = Logic.GetTime() + _length + 2
+end
