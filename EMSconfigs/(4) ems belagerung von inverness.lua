0369a3cc9281184e261b07d5b6b4a08060605bf4
Oliver Frahnert <oliver.frahnert@smail.emt.h-brs.de>
2023-11-17 01:33:20 +0100

    (4) EMS Belagerung von Inverness updated
    - random c stack overflow hoffentlich gefixt

--- EMSconfigs/(4) ems belagerung von inverness.lua
+++ EMSconfigs/(4) ems belagerung von inverness.lua
@@ -13,7 +13,7 @@ EMS_CustomMapConfig =
 	-- * Configuration File Version
 	-- * A version check will make sure every player has the same version of the configuration file
 	-- ********************************************************************************************
-	Version = 2.7,
+	Version = 3.0,
  
 	-- ********************************************************************************************
 	-- * Debug Mode
