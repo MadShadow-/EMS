bb3761c77bca40f063613dd3455fd5255e287ee1
Oliver Frahnert <oliver.frahnert@smail.emt.h-brs.de>
2023-11-19 12:26:38 +0100

    (4) EMS Belagerung von Inverness updated:
    v3.2 -> changed camp attacks

--- EMSconfigs/(4) ems belagerung von inverness.lua
+++ EMSconfigs/(4) ems belagerung von inverness.lua
@@ -13,7 +13,7 @@ EMS_CustomMapConfig =
 	-- * Configuration File Version
 	-- * A version check will make sure every player has the same version of the configuration file
 	-- ********************************************************************************************
-	Version = 3.1,
+	Version = 3.2,
  
 	-- ********************************************************************************************
 	-- * Debug Mode
