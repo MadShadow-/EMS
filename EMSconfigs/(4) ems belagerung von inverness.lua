616c8e20ccf2074f5117cfd1ca36ce03cf16e623
Oliver Frahnert <oliver.frahnert@smail.emt.h-brs.de>
2024-01-08 17:55:00 +0100

    Map updated: Belagerung von Inverness: Added SP Mode, Continue playing after win

--- EMSconfigs/(4) ems belagerung von inverness.lua
+++ EMSconfigs/(4) ems belagerung von inverness.lua
@@ -13,7 +13,7 @@ EMS_CustomMapConfig =
 	-- * Configuration File Version
 	-- * A version check will make sure every player has the same version of the configuration file
 	-- ********************************************************************************************
-	Version = 3.8,
+	Version = 4.0,
  
 	-- ********************************************************************************************
 	-- * Debug Mode
