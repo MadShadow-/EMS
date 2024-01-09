201b3a87e310d79ca7ab44c2546f788a8a312a55
Oliver Frahnert <oliver.frahnert@smail.emt.h-brs.de>
2024-01-09 20:06:21 +0100

    Map updated: Belagerung von Inverness: Added 1vs1 mode

--- EMSconfigs/(4) ems belagerung von inverness.lua
+++ EMSconfigs/(4) ems belagerung von inverness.lua
@@ -13,7 +13,7 @@ EMS_CustomMapConfig =
 	-- * Configuration File Version
 	-- * A version check will make sure every player has the same version of the configuration file
 	-- ********************************************************************************************
-	Version = 4.1,
+	Version = 4.2,
  
 	-- ********************************************************************************************
 	-- * Debug Mode
